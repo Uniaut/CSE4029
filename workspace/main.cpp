03ab4d7f6808f71baf308b508abc496c60bcb19d
Uniaut <kunwoo0927@gmail.com>
2022-12-03 22:28:10 +0900

    commit

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -1,7 +1,134 @@
 #include <iostream>
 #include "data.hpp"
+#include <algorithm>
 
 using namespace std;
+
+// vector is sorted. use intersection algorithm
+int interest_fitness(vector <Interest> interest1, vector <Interest> interest2)
+{
+    int fitness = 0;
+    int i = 0, j = 0;
+    while (i < interest1.size() && j < interest2.size())
+    {
+        if (interest1[i] < interest2[j])
+            i++;
+        else if (interest1[i] > interest2[j])
+            j++;
+        else
+        {
+            fitness++;
+            i++;
+            j++;
+        }
+    }
+    return fitness;
+}
+
+int combination_fitness(UserData *a, UserData *b)
+{
+    int mbti_fit = MBTICombination[a->mbti][b->mbti];  
+    int interest_fit =0;
+    int result =0;
+    interest_fit = interest_fitness(a->interests, b->interests);
+    // mbti fit 0 ~ 4 / interet_fit 0 ~ 10 5 : 2 5 : 1 10 :2 
+    result = 8 * mbti_fit + 2 * interest_fit; 
+    // cout << "mbti_fit : " << mbti_fit << " interest_fit : " << interest_fit << " result : " << result << endl;
+    return result;
+}
+
+// heap sort
+// input: array of UserData
+// output: sorted array of UserData by fitness
+UserData** heapsortAsFitness(UserData **unsorted, int index_query, int size)
+{
+    UserData **sorted = new UserData*[size];
+    UserData *center = unsorted[index_query];
+    for (int i = 0; i < size; i++)
+    {
+        sorted[i] = unsorted[i];
+    }
+
+    for (int i = 1; i < size; i++)
+    {
+        int child = i;
+        while (child > 0)
+        {
+            int parent = (child - 1) / 2;
+            if (combination_fitness(center, sorted[parent]) < combination_fitness(center, sorted[child]))
+            {
+                UserData *temp = sorted[parent];
+                sorted[parent] = sorted[child];
+                sorted[child] = temp;
+            }
+            child = parent;
+        }
+    }
+    
+    return sorted;
+}
+
+
+// merge sort
+// input: array of UserData
+// output: sorted array of UserData by fitness
+UserData** mergesortAsFitness(UserData **unsorted, int index_query, int size)
+{
+    UserData **sorted = new UserData*[size];
+    UserData *center = unsorted[index_query];
+    for (int i = 0; i < size; i++)
+    {
+        sorted[i] = unsorted[i];
+    }
+
+    for (int i = 1; i < size; i *= 2)
+    {
+        for (int j = 0; j < size - i; j += i * 2)
+        {
+            int left = j;
+            int right = j + i;
+            int end = j + i * 2;
+            if (end > size)
+                end = size;
+            UserData **temp = new UserData*[end - left];
+            int index = 0;
+            while (left < j + i && right < end)
+            {
+                if (combination_fitness(center, sorted[left]) > combination_fitness(center, sorted[right]))
+                {
+                    temp[index] = sorted[left];
+                    left++;
+                }
+                else
+                {
+                    temp[index] = sorted[right];
+                    right++;
+                }
+                index++;
+            }
+            while (left < j + i)
+            {
+                temp[index] = sorted[left];
+                left++;
+                index++;
+            }
+            while (right < end)
+            {
+                temp[index] = sorted[right];
+                right++;
+                index++;
+            }
+            for (int k = j; k < end; k++)
+            {
+                sorted[k] = temp[k - j];
+            }
+            delete[] temp;
+        }
+    }
+    return sorted;
+}
+
+
 
 
 int main()
@@ -11,13 +138,14 @@ int main()
 
     UserData *UserSet[100];
     
-    for( int i =0 ; i< 100; i++){
+    for(int i = 0 ; i< 100; i++){
         
         UserSet[i] = randomGenerator(i);
         cout << "User ID: " << UserSet[i]->id << endl;
         cout << "User MBTI: " << UserSet[i]->mbti << endl;
         cout << "User Interests: "; 
-        for(int j = 0; j<UserSet[i]->interests.size(); j++){
+        for(int j = 0; j<UserSet[i]->interests.size(); j++)
+        {
             cout << UserSet[i]->interests[j] << "/";
         }
         cout << endl;
@@ -25,10 +153,24 @@ int main()
     // 알고리즘 별 결과 확인 
     // 1. MBTI별로 궁합도 확인하여 정렬 후 3, 4점인 사람들만 데이터 추출하여 새로운 임시 배열에 저장.
     // 2. 1번에서 만든 데이터들 중에 관심사가 같은 사람들 확인하여 결과 출력.
-    // 3. ? 
+    // 3. ????????????????????????????????????????????????????
 
-    
-    // Heap Sort
+    cout << "Algorithm 1: Merge Sort" << endl;
+    auto result = mergesortAsFitness(UserSet, 3, 100);
+    for(int i = 0 ; i< 100; i++)
+    {
+        cout << "User ID: " << result[i]->id << '\t';
+        cout << "Fitness: " << combination_fitness(result[i], UserSet[3]) << endl;
+    }
+
+    cout << "Algorithm 1: Merge Sort" << endl;
+    auto result = mergesortAsFitness(UserSet, 3, 100);
+    for(int i = 0 ; i< 100; i++)
+    {
+        cout << "User ID: " << result[i]->id << '\t';
+        cout << "Fitness: " << combination_fitness(result[i], UserSet[3]) << endl;
+    }
+    //  //Heap Sort
 	// clock_t start, end;
 	// double result;
     // start = clock();
